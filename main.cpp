6f257e785d9851ee6477d778eddf57340528cfa4
Karol <kurdakarol@gmail.com>
2022-04-10 23:20:46 +0200

    test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     MinesweeperBoard board (2, 2, GameMode::EASY);
     board.debug_display();
-    MSBoardTextView view (board);
+    MSBoardTextView view (bofdsard);
     MSTextController ctrl (board, view );
 
     ctrl.play();
